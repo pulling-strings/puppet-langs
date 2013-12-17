dab115f7f546dff2d0ab3326c31004362e40e80a
narkisr <narkisr@gmail.com>
2013-12-18 01:46:35 +0200

    moving to jdk 7 by default

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,11 @@
 # A class which sets up programming languages
 class langs {
-  include jdk
   include ruby
   include langs::erlang
   include coffeescript
   include build_essential
+
+  class{'jdk':
+    version => 7
+  }
 }
