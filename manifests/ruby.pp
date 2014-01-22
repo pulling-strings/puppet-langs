47f3cfa60fb395360fd65da00e82a60f9e0237f6
narkisr <narkisr@gmail.com>
2014-01-23 00:47:37 +0200

    moving to ruby 2.1.0

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -18,7 +18,7 @@ class langs::ruby {
 
   rvm::system_user { $username: }
 
-  $ruby9 = 'ruby-1.9.3'
+  $ruby2 = 'ruby-2.1.0'
 
   if $rvm_installed=="true" {
     package{['libgdbm-dev', 'libncurses5-dev', 'libtool',
@@ -27,18 +27,18 @@ class langs::ruby {
       } ->
 
       rvm_system_ruby {
-        $ruby9:
+        $ruby2:
           ensure      => 'present',
           default_use => true;
       }
 
       rvm_gem {
         'bundler':
-          ruby_version => $ruby9,
-          require      => Rvm_system_ruby[$ruby9];
+          ruby_version => $ruby2,
+          require      => Rvm_system_ruby[$ruby2];
         'rake':
-          ruby_version => $ruby9,
-          require      => Rvm_system_ruby[$ruby9];
+          ruby_version => $ruby2,
+          require      => Rvm_system_ruby[$ruby2];
       }
   }
 }
