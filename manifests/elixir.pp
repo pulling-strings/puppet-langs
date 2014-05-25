66611c14d5cf51a1c69326c2f3a263147d75105d
narkisr <narkisr@gmail.com>
2014-05-26 01:27:23 +0300

    latest elixir

--- manifests/elixir.pp
+++ manifests/elixir.pp
@@ -1,6 +1,6 @@
 # setting up elixir lang
 class langs::elixir {
-  $version = 'v0.12.4'
+  $version = 'v0.13.3'
 
   ensure_resource('package', 'unzip', {'ensure' => 'present' })
 
@@ -13,25 +13,25 @@ class langs::elixir {
   archive { $version:
     ensure        => present,
     url           => $url,
-    digest_string => 'c6a41e761ee26687ec0939d666e0bc07',
+    digest_string => 'b7205f0d8ba3fecbd646d33f5da9cba1',
     src_target    => '/usr/src',
-    target        => "/usr/share/elixir-${version}",
+    target        => "/usr/share/elixir",
     extension     => 'zip',
     require       => Package['unzip'],
   }
 
   file{'/usr/bin/mix':
     ensure => link,
-    target => "/usr/share/elixir-${version}/bin/mix"
+    target => "/usr/share/elixir/${version}/bin/mix"
   }
 
   file{'/usr/bin/iex':
     ensure => link,
-    target => "/usr/share/elixir-${version}/bin/iex"
+    target => "/usr/share/elixir/${version}/bin/iex"
   }
 
   file{'/usr/bin/elixir':
     ensure => link,
-    target => "/usr/share/elixir-${version}/bin/elixir"
+    target => "/usr/share/elixir/${version}/bin/elixir"
   }
 }
