0e25feaca4dec3f271421d44ef5bd0c6de1aff92
ronen <narkisr@gmail.com>
2015-07-26 16:42:19 +0300

    using nodesource repo

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,7 +1,23 @@
 # Setting up nodejs
 class langs::node {
 
-  apt::ppa {'ppa:chris-lea/node.js':} ->
+  apt::ppa {'ppa:chris-lea/node.js':
+    ensure => absent
+  }
+
+  apt::source { 'nodesource':
+      location    => 'https://deb.nodesource.com/node_0.10',
+      release     => 'vivid',
+      repos       => 'main',
+      include_src => true,
+  }
+
+  apt::key {'nodesource':
+    key        => '1655A0AB68576280',
+    key_source => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+  } ->
+
+  Exec['apt_update'] ->
 
   package{'nodejs':
     ensure  => present
