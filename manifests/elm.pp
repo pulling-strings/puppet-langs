eafc03796519eb32dc48cef4aaf0ddcd6272d186
narkisr <narkisr@gmail.com>
2015-12-07 17:05:31 +0200

    adding version support

--- manifests/elm.pp
+++ manifests/elm.pp
@@ -1,7 +1,9 @@
 # Setting up elm-lang.org
-class langs::elm {
+class langs::elm(
+  $version = '0.16'
+){
   exec{'install elm':
-    command => 'npm install -g elm@0.15.1',
+    command => "npm install -g elm@${version}",
     user    => 'root',
     path    => ['/usr/bin','/bin',],
     require => Package['nodejs']
