f352c1ef5e86618cf5d2d29e3f5e9c0dada8ca40
ronen <narkisr@gmail.com>
2016-02-13 03:18:07 +0200

    moving to 1.5.3

--- manifests/go.pp
+++ manifests/go.pp
@@ -1,16 +1,16 @@
 # Setting up go lang
 class langs::go {
-  $version = '1.5'
+  $version = '1.5.3'
   $release = "go${version}.linux-amd64"
   $url = "https://storage.googleapis.com/golang/${release}.tar.gz"
   $target = '/usr/local/'
-  $sum = '5817fa4b2252afdb02e11e8b9dc1d9173ef3bd5a'
+  $sum = '43afe0c5017e502630b1aea4d44b8a7f059bf60d7f29dfd58db454d4e4e0ae53'
 
   archive {$release:
     ensure        => present,
     url           => $url,
     digest_string => $sum,
-    digest_type   => 'sha1',
+    digest_type   => 'sha256',
     src_target    => '/usr/src',
     target        => $target,
     extension     => 'tar.gz',
