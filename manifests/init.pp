f40e55d42ac1e3b27050179c1bd84d0db239f266
ronen <narkisr@gmail.com>
2018-03-17 16:13:19 +1100

    moving to node 8x

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 # A class which sets up programming languages
 class langs {
   include langs::ruby
-  include langs::elm
   include langs::node
   include langs::go
   include langs::build_essential
--- manifests/node.pp
+++ manifests/node.pp
@@ -5,23 +5,33 @@ class langs::node {
     ensure => absent
   }
 
-  apt::source { 'nodesource':
+  apt::source { 'node-4x':
+      ensure   => absent,
       location => 'https://deb.nodesource.com/node_4.x',
       release  => 'xenial',
       repos    => 'main',
       include  => {
-        src => true,
+        src    => true,
+      }
+  }
+
+  apt::source { 'node-8x':
+      location => 'https://deb.nodesource.com/node_8.x',
+      release  => 'xenial',
+      repos    => 'main',
+      include  => {
+        src    => true,
       }
   }
 
   apt::key {'nodesource':
     id     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
     source => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
-  } ->
+  }
 
-  Exec['apt_update'] ->
+  -> Exec['apt_update']
 
-  package{'nodejs':
+  -> package{'nodejs':
     ensure  => present
   }
 }
